7d3bc15582eb7c881fd55bbc7262d9d57e6445ae
muiruri3000 <josee.muiruri95@gmail.com>
2024-12-14 11:34:11 +0300

    m

--- src/Bmi.jsx
+++ src/Bmi.jsx
@@ -5,11 +5,11 @@ const Bmi = () => {
     <div className="row mt-3 container bmi align-items-center
     mx-auto">
         <div className="col-md-6 col-sm-12">
-            <img src="./images/image-man-eating.webp" className="img-fluid" alt="" />
+            <img src="./images/image-man-eating.webp" className="img-fluid" alt="man eating" />
         </div>
         <div className="col-md-6 col-sm-12">
         <h2 className="my-3">What your BMI result means</h2>
-<p className="">
+<p >
 A BMI range of 18.5 to 24.9 is considered a healthy weight. Maintaining a 
 healthy weight may lower your chances of experiencing health issues later on, 
 such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced 
