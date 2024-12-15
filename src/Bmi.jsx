73ca16d8963707f9a709edba1d3578d160eabb30
muiruri3000 <josee.muiruri95@gmail.com>
2024-12-15 10:37:59 +0300

    corrected paragraph content

--- src/Bmi.jsx
+++ src/Bmi.jsx
@@ -98,48 +98,53 @@ five days a week.
         
       <div className="card p-3 shadow text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3">
-                    <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Racer</h5>
+                    <img src="./images/icon-gender.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
+                    <h5 className="mb-0">Gender</h5>
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
+                  <p>  The development and body fat composition of girls and boys vary with age. Consequently, 
+                  a child's age and gender are considered when evaluating their BMI.</p>
                 </div>
         </div> 
 
         <div className="col-12 col-sm-6 col-md-4 d-none d-lg-block col-lg-4 mx-auto">
                 <div className="card shadow p-3 text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3 ">
-                    <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Race Glider</h5>
+                  <img src="./images/pattern-curved-line-left.svg" alt="" />
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
+                  
                 </div>
               </div>
               <div className="col-12 col-sm-6  col-md-6 col-lg-4 ">
                 <div className="card shadow  p-3 text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3">
-                    <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Race Wider</h5>
+                    <img src="../public/images/icon-age.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
+                    <h5 className="mb-0">Age</h5>
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
+                  <p>  In aging individuals, increased body fat and muscle loss may cause BMI to underestimate 
+                  body fat content.</p>
                 </div>
               </div>
             <div className="col-12 col-sm-6 col-lg-4 col-md-6 col-sm-4 mx-auto">
                 <div className="card shadow p-3 text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3">
-                    <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Race Minor</h5>
+                    <img src="./images/icon-muscle.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
+                    <h5 className="mb-0">Muscle</h5>
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
+                  <p>  BMI may misclassify muscular individuals as overweight or obese, as it doesn't 
+                  differentiate muscle from fat.</p>
                 </div>
               </div>
 
               <div className="col-12 col-sm-6  col-md-6 col-lg-4">
                 <div className="card shadow  p-3 text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3">
-                    <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Race</h5>
+                    <img src="../public/images/icon-pregnancy.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
+                    <h5 className="mb-0">Pregnancy</h5>
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
+                 <p>Expectant mothers experience weight gain due to their growing baby. Maintaining a 
+  healthy pre-pregnancy BMI is advisable to minimise health risks for both mother 
+  and child.</p>
+               
                 </div>
               </div>
               
@@ -147,10 +152,13 @@ five days a week.
                 <div className="card shadow p-3 text-center">
                   <div className="d-flex align-items-center justify-content-center mb-3">
                     <img src="./images/icon-race.svg" alt="Race Icon" className="img-fluid icon-image me-2"/>
-                    <h5 className="mb-0">Racee</h5>
+                    <h5 className="mb-0">Race</h5>
                   </div>
-                  <p>Certain health concerns may affect individuals of some Black and Asian origins...</p>
-                </div>
+                <p>  Certain health concerns may affect individuals of some Black and Asian origins at 
+  lower BMIs than others. To learn more, it is advised to discuss this with your 
+  GP or practice nurse.
+</p>
+               </div>
               </div>
               
      
